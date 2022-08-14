e4594ae0a91cf8e502c923e0c72c75f71f9bcde3
CrioUser <crio-user@criodo.com>
2022-08-14 20:12:08 +0530

    changing StandardCalc.java

--- src/main/java/com/crio/qcalc/StandardCalculator.java
+++ src/main/java/com/crio/qcalc/StandardCalculator.java
@@ -6,4 +6,23 @@ public class StandardCalculator {
         System.out.println("Standard Calculator 1.0");
     }
 
+    public int add(int num1, int num2) {
+        return (num1 + num2);
+    }
+
+
+    public int subtract(int num1, int num2) {
+        return (num1 > num2) ? (num1 - num2) : (num2 - num1);
+    }
+
+
+    public int multiply(int num1, int num2) {
+        return (num1 * num2);
+    }
+
+
+    public int divide(int num1, int num2) {
+        return (num1/num2);
+    }
+
 }
